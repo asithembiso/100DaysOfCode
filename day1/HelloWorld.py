d8f91a3d05d24f5d4ecbdad8cdd89e6a228f822f
Athi Sithembiso <asithembiso@gmail.com>
2024-10-10 12:46:40 +0200

    Added comments

--- day1/HelloWorld.py
+++ day1/HelloWorld.py
@@ -1,7 +1,13 @@
+"""
+    This is a multi line
+    comment
+"""
+
 
 def hello_world() -> None:
     print("Hello World!")
 
 
+# This is a single line comment
 if __name__ == "__main__":
     hello_world()
