803787bedfeead0cd862bb5dac57bb9df6505ff7
Athi Sithembiso <asithembiso@gmail.com>
2024-10-15 12:22:18 +0200

    Day 3 of 100 days of code, inputs

---
+++ day3/inputs.py
@@ -0,0 +1,8 @@
+
+name = str(input("What is your name? "))
+print(f"Hello {name}!")
+
+age = int(input("How old are you? "))
+grade_avg = float(input("What is your GPA? "))
+
+print(f"\n\n{name} \nAge: {age} \nGPA:{grade_avg}")
