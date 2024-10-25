19a9a1045f75039d4fc870bfbc3af042b3abec4e
Athi Sithembiso <asithembiso@gmail.com>
2024-10-25 17:09:29 +0200

    Day 9 random num generator

---
+++ day-09/random_num_generator.py
@@ -0,0 +1,13 @@
+"""
+1. Write a program that generates a random number.
+2. Write a program that generates random number between 2 integers
+"""
+import random
+
+# Rando number
+print(random.random())
+
+# Random integer between 1 and 10 (inclusive)
+random_int = random.randint(1, 10)
+print(random_int)
+
